10936388c3e9053580dd04c8972a2198554e4557
Kevin Valdebenito <ke.valdebenitol@duocuc.cl>
2026-08-28 00:42:39 -0400

    Add HTML pages and JavaScript for employee management and login

---
+++ js/inicio-sesion.js
@@ -0,0 +1,21 @@
+
+const botonInicioSesion=document.getElementById('btn-login');
+
+botonInicioSesion.addEventListener('click',function(){
+    const inputEmail=document.getElementById('input-email');
+    const inputPassword=document.getElementById('input-password');
+
+    //aqui va una validacion de email y contraseña, si es correcta se redirige a la pagina de inicio, si no es correcta se muestra un mensaje de error
+
+    const email=inputEmail.value;
+    
+    if(email === 'admin@example.cl'){
+        window.location='admin/index.html';
+        localStorage.setItem('rol_usuario','admin');
+    }else if (email === 'empleado@tienda.cl'){
+        window.location='admin/empleado.html';
+        localStorage.setItem('rol_usuario','empleado');
+    } else {
+        alert('Credenciales incorrectas. Por favor, inténtelo de nuevo.');
+    }
+})
