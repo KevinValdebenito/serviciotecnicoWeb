881bf122dc1fbcae3d81e829f0b6e9c426e59960
benjasepulvedar-prog <benja.sepulvedar@duocuc.cl>
2026-09-13 01:39:53 -0300

    Mejora estructura HTML, navegación y validaciones del sitio

--- js/inicio-sesion.js
+++ js/inicio-sesion.js
@@ -1,21 +1,21 @@
-
 const botonInicioSesion=document.getElementById('btn-login');
 
 botonInicioSesion.addEventListener('click',function(){
-    const inputEmail=document.getElementById('input-email');
-    const inputPassword=document.getElementById('input-password');
 
-    //aqui va una validacion de email y contraseña, si es correcta se redirige a la pagina de inicio, si no es correcta se muestra un mensaje de error
+    const inputEmail=document.getElementById('input-email');
 
     const email=inputEmail.value;
-    
-    if(email === '  '){
+
+    if(email === ''){
+        alert('Debe ingresar su correo electrónico.');
+    }else if(email === 'admin@example.cl'){
         window.location='admin/indexAdmin.html';
         localStorage.setItem('rol_usuario','admin');
-    }else if (email === 'empleado@tienda.cl'){
+    }else if(email === 'empleado@tienda.cl'){
         window.location='empleado/indexEmpleado.html';
         localStorage.setItem('rol_usuario','empleado');
-    } else {
-        alert('Credenciales incorrectas. Por favor, inténtelo de nuevo.');
+    }else{
+        alert('Correo incorrecto. Por favor, inténtelo de nuevo.');
     }
+
 })
